6237f1efa495a304be704fce25f90fc7c2c8fda8
Дмитрий <iamsda@yandex.ru>
2023-09-29 18:49:38 +0300

    Добавлена программа квадрата числа

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+//Console.WriteLine (a*a);
+Console.WriteLine ($"Квадрат числа {a} равен {a*a}");
+
+//Вариант 2 - Математический 
+// int sqr = a * a;
+// Console.WriteLine (sqr);
+
+
