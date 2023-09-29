969835d280884caa26125c8f84240775982bfc8a
Дмитрий <iamsda@yandex.ru>
2023-09-29 19:01:36 +0300

    Добавленны альтернативные методы решения 1 задачи

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine()!);
-//Console.WriteLine (a*a);
-Console.WriteLine ($"Квадрат числа {a} равен {a*a}");
-
 //Вариант 2 - Математический 
-// int sqr = a * a;
-// Console.WriteLine (sqr);
+//int sqr = a * a;
+//Console.WriteLine ($"Квадрат числа {a} равен {sqr}");
 
-
+//Вариант 3 -  Библиотека
+int sqr1 = Convert.ToInt32(Math.Pow(a, 2));
+Console.WriteLine ($"Квадрат числа {a} равен {sqr1}");
