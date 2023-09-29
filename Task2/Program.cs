49f0a5d332ba8e79baaf39d41686134a5c2ecc26
Дмитрий <iamsda@yandex.ru>
2023-09-29 19:34:15 +0300

    Добавлена задача на проверку квадрата числа

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int first = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine()!);
+
+if (first == Convert.ToInt32(Math.Pow(second, 2)))
+{
+    Console.WriteLine($"Число {first} является квадратом числа {second}");
+}
+else Console.WriteLine($"Число {first} не является квадратом числа {second}");
